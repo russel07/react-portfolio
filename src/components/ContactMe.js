f49460fd8e5acb4f63160ab516a4912f8c705c43
Md. Russel Hussain <md.russel.hussain@gmail.com>
2021-09-25 23:39:30 +0600

    top nav bar added component created for all pages

---
+++ src/components/ContactMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './Header';
+import Footer from './Footer';
+
+function ContactMe(){
+    return (
+        <div>
+            <Header/>
+                <p>This is ContactMe page</p>
+            <Footer/>
+        </div>
+    )
+}
+export default ContactMe
---
+++ src/components/Resume.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './Header';
+import Footer from './Footer';
+
+function Resume(){
+    return (
+        <div>
+            <Header/>
+                <p>This is Resume page</p>
+            <Footer/>
+        </div>
+    )
+}
+export default Resume
